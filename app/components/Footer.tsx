02fa1d143cbdb22cb10a00e2f045f97b3cb06cb4
itumo-arigatone <soganokasegi0825@gmail.com>
2023-08-02 22:48:24 +0900

    add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="center mt-5 flex justify-center space-x-4 bg-sub text-base p-4 text-xs">
+        <div>Copyright © 2023 Itsumoarigatone. All Rights Reserved.</div>
+      </footer>
+    )
+  }
+  
+  export default Footer
