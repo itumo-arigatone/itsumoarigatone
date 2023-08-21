4fefc5fd85f84a3d2fe21bff86631af8259cd121
itumo-arigatone <soganokasegi0825@gmail.com>
2023-08-21 23:14:44 +0900

    change color code.

--- app/product_info.ts
+++ app/product_info.ts
@@ -16,21 +16,21 @@ const ProductsInfo: PItype = {
         price: 15000,
         image_path: "/logo_small.svg",
         description: "二つ折り財布になります。\nルガトーという革を使っています。全体的にちょっとキツめだけれど使っていくうちに馴染むかも？\n小銭入れはありません。",
-        color: ["#006400"],
+        color: ["#002e1a"],
     },
     green_tray: {
         name: "緑のトレー",
         price: 7000,
         image_path: "/logo_small.svg",
         description: "大人な雰囲気の深緑色トレー\n無造作に置かれた鍵などをこのトレーの上に載せたらそれだけで整理されたように見えます。",
-        color: ["#006400"],
+        color: ["#002e1a"],
     },
     code_clip: {
         name: "本革のコードクリップ 8cm",
         price: 300,
         image_path: "/logo_small.svg",
         description: "本革のコードクリップになります。長さは8cmです。USB type-Cを通しておくことができます。",
-        color: ["#006400", "#000000"],
+        color: ["#002e1a", "#000000"],
     },
 };
 export default ProductsInfo;
