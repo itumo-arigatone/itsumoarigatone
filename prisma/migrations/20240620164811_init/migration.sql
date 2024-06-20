3c7884a880f2e77281e813124f72c112441d33fd
itumo-arigatone <soganokasegi0825@gmail.com>
2024-06-21 01:50:34 +0900

    add prisma

---
+++ prisma/migrations/20240620164811_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "post_pkey" PRIMARY KEY ("id")
+);
