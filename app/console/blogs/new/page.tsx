0c933f5dfdfb709a0e497e53ccfaf603137ea8c8
itumo-arigatone <soganokasegi0825@gmail.com>
2024-07-18 02:39:08 +0900

    control s3 file upload

--- app/console/blogs/new/page.tsx
+++ app/console/blogs/new/page.tsx
@@ -6,6 +6,20 @@ import { replaceImgSrc } from '@/lib/replaceImgSrc';
 import { uploadImages } from '@/lib/uploadImages';
 import '@/app/stylesheets/console/blogs/page.css'
 
+const syncFile = (imageKeys: string[], imageFiles: File[]) => {
+  const result = [];
+  for (const key of imageKeys) {
+    for (let i = 0; i < imageFiles.length; i++) {
+      const file = imageFiles[i];
+      if (file.name === key) {
+        result.push(file);
+        break;
+      }
+    }
+  }
+  return result;
+}
+
 async function PostBlog(data: FormData) {
   'use server'
 
@@ -23,11 +37,11 @@ async function PostBlog(data: FormData) {
   // タグからキーを取得する
   const imageKeys: string[] = [];
   domContent.querySelectorAll('img.uploaded-image').forEach((img: HTMLElement) => {
-    const key = img.getAttribute('alt');
+    const key = img.getAttribute('alt')
     if (key) {
-      imageKeys.push(key);
+      imageKeys.push(key)
     }
-  });
+  })
 
   const result = await prisma.post.create({
     data: {
@@ -43,8 +57,8 @@ async function PostBlog(data: FormData) {
 
   if (result) {
     // s3にアップロード
-    console.log(imageFiles)
-    await uploadImages(`blog/${result.id}/`, imageFiles)
+    const syncedFiles = syncFile(imageKeys, imageFiles)
+    await uploadImages(`blog/${result.id}/`, syncedFiles)
     redirect('/console/blogs');
   }
 }
