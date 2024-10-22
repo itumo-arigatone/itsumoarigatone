93af1433414fbfcf14286e19428fa2dfa63e3eb1
itumo-arigatone <soganokasegi0825@gmail.com>
2024-10-22 22:37:18 +0900

    add change endpoint change process

--- lib/s3Client.ts
+++ lib/s3Client.ts
@@ -1,14 +1,19 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export function s3Client(): S3Client {
+  const isProduction = process.env.NODE_ENV === 'production';
+
   let s3Client = new S3Client({
     region: 'ap-northeast-1',
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'minioadmin' as string,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'minioadmin' as string,
     },
-    endpoint: 'http://minio:9000', // TODO: product_endpoint
     forcePathStyle: true,
-  })
-  return s3Client
+    ...(isProduction ? {} : {
+      endpoint: 'http://minio:9000',
+    }),
+  });
+
+  return s3Client;
 }
--- lib/viewS3Client.ts
+++ lib/viewS3Client.ts
@@ -1,14 +1,18 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export function viewS3Client() {
+  const isProduction = process.env.NODE_ENV === 'production';
+
   let s3 = new S3Client({
     region: 'ap-northeast-1',
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'minioadmin' as string,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'minioadmin' as string,
     },
-    endpoint: 'http://127.0.0.1:9000',
     forcePathStyle: true,
+    ...(isProduction ? {} : {
+      endpoint: 'http://127.0.0.1:9000',
+    }),
   });
   return s3;
 }
