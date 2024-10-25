ff3dcf7f803e0cf89c25fbacc48555bd5c7e6d5f
itumo-arigatone <soganokasegi0825@gmail.com>
2024-10-26 04:39:21 +0900

    can display image

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,7 +2,12 @@ import Footer from "@/app/(components)/Footer";
 import Header from "@/app/(components)/SimpleHeader";
 import SafeHTML from '@/app/(components)/SafeHTML';
 import '/app/stylesheets/blog/detail_page.css';
+import { parse } from 'node-html-parser';
 import { PrismaClient } from '@prisma/client';
+import { viewS3Client } from "@/lib/viewS3Client"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { replaceImgSrc } from '@/lib/replaceImgSrc';
 import { use } from 'react';
 
 interface Post {
@@ -10,9 +15,14 @@ interface Post {
   content: string;
 }
 
+interface ImgSrc {
+  [src: string]: string;
+}
+
 async function GetBlog(id: string) {
   'use server'
 
+  const Bucket = process.env.AMPLIFY_BUCKET;
   const prisma = new PrismaClient();
 
   if (!id) {
@@ -27,6 +37,22 @@ async function GetBlog(id: string) {
     return { error: 'Post not found' };
   }
 
+  const domContent = parse(post.content)
+
+  // タグからキーを取得する
+  let imgSrc: ImgSrc = {}
+  const imgElements = domContent.querySelectorAll('img.uploaded-image');
+
+  for (const img of imgElements) {
+    const key = img.getAttribute('alt');
+
+    if (key) {
+      const command = new GetObjectCommand({ Bucket, Key: `blog/${id}/${key}` });
+      imgSrc[key] = await getSignedUrl(viewS3Client(), command, { expiresIn: 3600 });
+    }
+  }
+
+  post.content = replaceImgSrc(domContent, imgSrc)
   return post;
 }
 
