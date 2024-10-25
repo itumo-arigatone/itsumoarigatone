89e4d8c82a694e296721e924a5309d7c6316071f
itumo-arigatone <soganokasegi0825@gmail.com>
2024-10-26 08:59:16 +0900

    fix simple header style

--- app/(components)/SimpleHeader.tsx
+++ app/(components)/SimpleHeader.tsx
@@ -2,7 +2,7 @@ import '../stylesheets/header.scss'
 
 export default function SimpleHeader() {
   return (
-    <header className="relative center flex justify-center space-x-4 bg-sub p-4 text-xs">
+    <header className="simple-header relative center flex justify-center space-x-4 bg-sub p-4 text-xs">
       <a href="/">
         <img src="/logo_medium.svg" alt="Itsumoarigatone" width={150} height={47} />
       </a>
