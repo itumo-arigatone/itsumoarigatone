6b41229bad4e615419182436fe509b262ca3441a
soma sato <65723443+itumo-arigatone@users.noreply.github.com>
2024-11-02 00:45:02 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QL49PN4YYY"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-QL49PN4YYY');
+        </script>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
